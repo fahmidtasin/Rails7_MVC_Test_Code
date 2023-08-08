86f1763a30ca188092ae81b5421b0f12a05a70d9
Fahmid Shafath Tasin <fahmidtasin@gmail.com>
2023-08-08 15:58:40 -0700

    login logout test pending

--- spec/views/posts/index.html.erb_spec.rb
+++ spec/views/posts/index.html.erb_spec.rb
@@ -13,21 +13,33 @@ RSpec.describe "posts/index", type: :view do
 
   before(:each) do
     assign(:posts, [post])
-    # Include Devise test helpers
     view.extend Devise::Test::ControllerHelpers
-    # Manually set the current_user variable
     allow(view).to receive(:current_user).and_return(user)
   end
 
   it "renders a list of posts" do
     render
 
-    # Check if the post title, content, and link to show are present in the view
     expect(rendered).to have_text("Title")
     expect(rendered).to have_text("MyText")
     expect(rendered).to have_link("Show this post", href: post_path(post))
 
-    # Check if the current user email is displayed
+    # Verify that the user's email is displayed in the view
     expect(rendered).to have_text("Current user: #{user.email}")
+
+    # Check if the Logout button is present when user is signed in
+    expect(rendered).to have_button("Logout")
+
+    # Check if the New post link is present
+    expect(rendered).to have_link("New post", href: new_post_path)
+  end
+
+  it "displays Login link when user is not signed in" do
+    # Clear the mocked current_user for this test
+    allow(view).to receive(:current_user).and_return(nil)
+    render
+
+    # Check if the Login link is present when user is not signed in
+    expect(rendered).to have_link("Login", href: new_user_session_path)
   end
 end
